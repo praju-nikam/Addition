5d77c8d84a39a44bce629127896208a8c174402b
praju <sawantprajakta84@gmail.com>
2022-02-22 07:50:15 +0530

    Initial commit

---
+++ src/Add.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Add
+{
+    public static void main(String args[])
+    {
+        int a,b,sum;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number : ");
+        a = input.nextInt();
+        System.out.println("Enter the second number");
+        b = input.nextInt();
+
+        input.close();
+        sum = a + b;
+        System.out.println("Addition = " +sum);
+
+    }
+}
